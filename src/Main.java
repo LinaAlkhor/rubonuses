0f8b6272e2401ae44198a26407e27628c5037baa
Lina Alkhor <lina.alkhor@gmail.com>
2023-09-29 21:01:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int added = 400;
+        int bonus = 0;
+                if (added > 1000) {
+            bonus = added / 100;;
+        }
+        int finalBalance = balance + added + bonus;
+        System.out.println("Итог" + finalBalance);
+        System.out.println("Бонус" + bonus);
+    }
+}
